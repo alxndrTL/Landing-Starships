bc8bc8bab9d278e235ef6e26371f554c5394d45b
Alexandre TL <alextorresleguet@icloud.com>
2020-08-27 09:49:23 +0200

    SN20

---
+++ SN20/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animation))]
+public class AnimationController : MonoBehaviour
+{
+
+    private Animation anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.Play("LandingLegsDeploy");
+        }
+    }
+}
